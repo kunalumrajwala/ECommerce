d8723467eb3030b584dd24c10d1b8372d87a7574
kunalumrajwala <kunalumrajwala@gmail.com>
2020-07-14 18:40:49 +0530

    Modify Angular Project

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
+import {IProduct } from './Model/IProduct';
+import {IPagination } from './Model/Pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'Ecommerce';
+  products: IProduct[];
+
+  constructor(private _httpClient: HttpClient){
+
+  }
+
+  ngOnInit(): void {
+    this._httpClient.get('https://localhost:5001/api/products').subscribe((response: IPagination) => {
+      this.products = response.data;
+    }, error => {
+      console.log(error);
+    });
+  }
 }
